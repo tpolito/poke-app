ee3432624c7dac9048f4ddde258c2416e3711c90
tpolito <0613.tyler@gmail.com>
2018-08-09 18:54:33 -0500

    finished adding the pokemon typing functionality

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -3,24 +3,42 @@ import React, { Component } from "react";
 class Pokemon extends Component {
   render() {
     const { pokemon } = this.props;
+    // if (pokemon.pokemons.types && pokemon.pokemons.types[1]) {
+    //   console.log("has two types");
+    // } else {
+    //   console.log("has one type");
+    // }
+
     if (pokemon.pokemons.types && pokemon.pokemons.types[1]) {
-      console.log("has two types");
+      return (
+        <div>
+          <p>Name: {pokemon.pokemons.name}</p>
+          <p>#{pokemon.pokemons.id}</p>
+          {pokemon.pokemons.sprites && (
+            <img src={pokemon.pokemons.sprites.front_default} alt="" />
+          )}{" "}
+          {pokemon.pokemons.types && (
+            <p>Typing: {pokemon.pokemons.types[0].type.name}</p>
+          )}
+          {pokemon.pokemons.types && (
+            <p>Typing: {pokemon.pokemons.types[1].type.name}</p>
+          )}
+        </div>
+      );
     } else {
-      console.log("has one type");
+      return (
+        <div>
+          <p>Name: {pokemon.pokemons.name}</p>
+          <p>#{pokemon.pokemons.id}</p>
+          {pokemon.pokemons.sprites && (
+            <img src={pokemon.pokemons.sprites.front_default} alt="" />
+          )}{" "}
+          {pokemon.pokemons.types && (
+            <p>Typing: {pokemon.pokemons.types[0].type.name}</p>
+          )}
+        </div>
+      );
     }
-    // console.log(pokemon.pokemons);
-    return (
-      <div>
-        <p>Name: {pokemon.pokemons.name}</p>
-        <p>#: {pokemon.pokemons.id}</p>
-        {pokemon.pokemons.sprites && (
-          <img src={pokemon.pokemons.sprites.front_default} alt="" />
-        )}{" "}
-        {pokemon.pokemons.types && (
-          <p>Type1: {pokemon.pokemons.types[0].type.name}</p>
-        )}
-      </div>
-    );
   }
 }
 
