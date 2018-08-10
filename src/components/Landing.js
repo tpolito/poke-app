a9f568b746ed6abc03549d1506b1c43dbecbd1a5
tpolito <0613.tyler@gmail.com>
2018-08-10 12:35:57 -0500

    Finished a search by # feature and worked on a little bit of styling

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,6 +6,12 @@ class Landing extends Component {
     id: ""
   };
 
+  onSubmit = e => {
+    const { id } = this.state;
+
+    this.props.history.push(`/pokemon/${id}`);
+  };
+
   onChange = e => this.setState({ [e.target.name]: e.target.value });
 
   render() {
@@ -13,18 +19,19 @@ class Landing extends Component {
     return (
       <div className="container">
         <div className="row">
-          <div className="col s8 offset-s2">
-            <h1>LANDING PAGE</h1>
-            <h2>
-              <Link to={`/pokemon/${id}`}>Pokemon</Link>
-            </h2>
-            <input
-              onChange={this.onChange}
-              type="text"
-              name="id"
-              maxLength="3"
-              required
-            />
+          <div className="input-field col s8 offset-s2">
+            <h1>Type # Of Pokemon</h1>
+
+            <form onSubmit={this.onSubmit}>
+              <label htmlFor="id">Pokemon Number</label>
+              <input
+                onChange={this.onChange}
+                type="text"
+                name="id"
+                maxLength="3"
+                required
+              />
+            </form>
           </div>
         </div>
       </div>
